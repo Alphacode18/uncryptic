18ba6c1a61281d6250d23f611c75cfcb15887371
Alphacode18 <skharban@purdue.edu>
2021-10-10 14:51:22 -0400

    feat: redirects output from lexer to the main method; adds test Makefile

--- system/main.c
+++ system/main.c
@@ -1,5 +1,37 @@
+#include <stdio.h>
 #include <uncryptic.h>
 
-int main() {
-  return OK;
+extern int yylex();
+extern int yylineno;
+extern char* yytext;
+
+char *names[] = {NULL, "db_type", "db_name", "db_table_prefix", "db_port"};
+
+int main(void) 
+{
+
+	int token;
+	while(token = yylex()) {
+    switch(token) {
+      case (TYPE):
+        printf("Type: %s\t", yytext);
+        break;
+      case (SCOPE):
+        printf("Scope: %s\t", yytext);
+        break;
+      case (BREAKING):
+        printf("Breaking: True\t");
+        break;
+      case (COLON):
+        printf("Colon: True\t");
+        break;
+      case (DESCRIPTION):
+        printf("Description: %s\t", yytext + 1);
+        break;
+      default:
+        printf("\nCommit message has a syntax error\n");
+        break;
+    }
+	}
+	return 0;
 }
